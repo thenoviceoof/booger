a179b2afff8af8dce5ae816d6f97a002a9151cf4
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-30 00:55:39 -0400

    Write some more tests for the short nosetests parser

--- booger_test.py
+++ booger_test.py
@@ -6,7 +6,6 @@
 # and you think this stuff is worth it, you can buy me a beer in
 # return
 # Nathan Hwang <thenoviceoof>
-# ----------------------------------------------------------------------------
 ################################################################################
 
 from unittest import TestCase
@@ -19,7 +18,32 @@ from booger import NOSE_DIV_WIDTH, NosetestsParser
 class NosetestsParserTest(TestCase):
     def setUp(self):
         self.parser = NosetestsParser()
-    def short_output_test(self):
+
+    def short_output_end_test(self):
+        '''
+        Make sure we recognise the end of the short output
+        '''
         inp = '=' * 70
         out, end = self.parser.parse_short_output(inp)
-        assert end == True
+        assert end == False
+    def short_output_ok_test(self):
+        '''
+        Recognize `msg ... ok` messages
+        '''
+        msg = 'msg ... ok'
+        out, end = self.parser.parse_short_output(msg)
+        assert out == 'ok'
+    def short_output_fail_test(self):
+        '''
+        Recognize `msg ... FAIL` messages
+        '''
+        msg = 'msg ... FAIL'
+        out, end = self.parser.parse_short_output(msg)
+        assert out == 'fail'
+    def short_output_error_test(self):
+        '''
+        Recognize `msg ... ERROR` messages
+        '''
+        msg = 'msg ... ERROR'
+        out, end = self.parser.parse_short_output(msg)
+        assert out == 'error'
