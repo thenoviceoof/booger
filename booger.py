a893ed1d66ce6c15cfa25dcdd371a7b2835a7f7c
Nathan Hwang <thenoviceoof@gmail.com>
2012-09-30 01:32:25 -0400

    update parser to handle short and long inputs

--- booger.py
+++ booger.py
@@ -20,20 +20,59 @@ import subprocess
 # nosetests output parser
 
 NOSE_DIV_WIDTH = 70
+SHORT_MAPPING = {
+    '.': 'ok',
+    'E': 'ERROR',
+    'F': 'FAIL'
+}
 
 class NosetestsParser(object):
     short_output = True
 
+    counts = {
+        'ok': 0,
+        'fail': 0,
+        'error': 0
+    }
+    def parse_short_long_output(self, s):
+        '''
+        Try to parse apart the line as a -v test output
+        Return test, status
+        '''
+        m = re.match('^(.*) ... (ok|FAIL|ERROR)$', s)
+        if m is None:
+            return None, None
+        msg = m.group(1)
+        status = m.group(2).lower()
+        self.counts[status] += 1
+        return msg, status
+    def parse_short_short_output(self, s):
+        '''
+        Try to parse apart the line as a -v0 output (not an actual option)
+        Return None
+        '''
+        m = re.match('^([.EF]+)$', s)
+        if m is None:
+            return
+        ss = m.group(1)
+        ss = ss.split('')
+        for s in ss:
+            self.counts[SHORT_MAPPING[s]] += 1
     def parse_short_output(self, s):
         '''
         Takes a line of output
-        Returns (status (ok, fail, error), end)
+        Returns (test, status (ok, fail, error), end)
         '''
         if re.match('=' * NOSE_DIV_WIDTH, s):
-            return None, True
-        m = re.match('^.* ... (ok|FAIL|ERROR)$', s)
-        status = m.group(1).lower()
-        return status, False
+            return None, None, True
+        # get the test and it's status
+        test, status = self.parse_short_long_output(s)
+        if status:
+            return test, status, False
+        # this merely updates the counts
+        self.parse_short_short_output(s)
+        if status:
+            return None, None, False
 
     def parse_long_output(self, s):
         '''
@@ -49,17 +88,13 @@ class NosetestsParser(object):
         - Long output (traceback / stdout / logging)
         '''
         if self.short_output:
-            status, end = self.parse_short_output(s)
+            test, status, end = self.parse_short_output(s)
             print s,
             if end:
                 self.short_output = False
         else:
             output = self.parse_long_output(s)
         return ''
-
-# this is a test function, so we can run `nosetests booger.py` to get output
-def run_test():
-    assert False
 
 ################################################################################
 # windowing stuff
