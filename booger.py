09c7aa17e6bc47f2df07a59d6806ed92f75e75c7
Nathan Hwang <thenoviceoof@gmail.com>
2012-10-01 00:21:29 -0400

    use curses.wrapper

--- booger.py
+++ booger.py
@@ -20,35 +20,35 @@ from nose.plugins import Plugin
 ################################################################################
 # windowing stuff
 
-class CursesInterface(object):
+def curses_main(scr):
+    try:
+        # wait for a character for only 0.1s
+        curses.halfdelay(1)
+        while 1:
+            c = scr.getch()
+            if c == ord('q'):
+                return
+            scr.refresh()
+    except KeyboardInterrupt:
+        return
+
+class CursesInterface(threading.Thread):
     def __init__(self):
-        self.curses_scr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        self.curses_scr.keypad(1)
-    def run_main(self):
-        self.thread = threading.Thread(target=self.main)
-        self.thread.run()
-    def main(self):
-        try:
-            # wait for a character for only 0.1s
-            curses.halfdelay(1)
-            while 1:
-                c = self.curses_scr.getch()
-                if c == 'q':
-                    return
-                self.curses_scr.refresh()
-        except KeyboardInterrupt:
-            return
-    def update_status(self, lst):
-        self.curses_scr.addstr(0,0, "thing")
-    def join(self):
-        self.thread.join()
-    def __del__(self):
-        self.curses_scr.keypad(0)
-        curses.nocbreak()
-        curses.echo()
-        curses.endwin()
+        super(CursesInterface, self).__init__()
+
+        # self.curses_scr = curses.initscr()
+        # curses.noecho()
+        # curses.cbreak()
+        # self.curses_scr.keypad(1)
+
+    def run(self):
+        curses.wrapper(curses_main)
+
+    # def __del__(self):
+    #     self.curses_scr.keypad(0)
+    #     curses.nocbreak()
+    #     curses.echo()
+    #     curses.endwin()
 
 ################################################################################
 # nose plugin
@@ -62,13 +62,14 @@ class BoogerPlugin(Plugin):
         }
         self.win = CursesInterface()
         super(BoogerPlugin, self).__init__(*args, **kwargs)
-        self.win.run_main()
+        self.win.start()
 
     ############################################################################
     # utils
 
     def post_test(self):
-        self.win.update_status([])
+        pass
+        # self.win.update_status([])
 
     ############################################################################
     # test outcome handler
@@ -84,6 +85,7 @@ class BoogerPlugin(Plugin):
 
     def finalize(self, result):
         self.win.join()
+        del self.win
     def report(self, stream):
         pass
         # return False
@@ -108,6 +110,4 @@ def test_test():
 if __name__ == "__main__":
     # nose.main(plugins=[BoogerPlugin()])
     c = CursesInterface()
-    # c.run_main()
-    c.main()
-    # c.join()
+    c.start()
