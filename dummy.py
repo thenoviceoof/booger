23f596865a5c335f1a17bb14d4aa889b311d2f28
Nathan Hwang <thenoviceoof@gmail.com>
2012-10-21 13:21:14 -0400

    Issue #2, create conditions in which we have to scroll

--- dummy.py
+++ dummy.py
@@ -19,6 +19,19 @@ def test_test4():
     time.sleep(1)
     assert False
 
+def test_test5():
+    assert False
+def test_test6():
+    assert False
+def test_test7():
+    assert False
+def test_test8():
+    assert False
+def test_test9():
+    assert False
+def test_test10():
+    assert False
+
 def mu_test():
     print "BANG BANG"
 
