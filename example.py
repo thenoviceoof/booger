22d637c09b8ab1f04966f983e74f33e75252770a
Nathan Hwang <thenoviceoof@gmail.com>
2014-05-01 23:03:14 -0400

    Exercise the box class a bit more in the example

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from ui import *
 # example usage
 
 class App(Application):
-    windows = {'default': Box(Text('hello world\nglad you could swing by'), title_parts=[' thing ', ' another '])}
+    windows = {'default': Box(Text('hello world\nglad you could swing by'), title_parts=[' thing ', ' another '], option_parts=[' thing '])}
 
     def handle(self, key):
         if key == 'a':
