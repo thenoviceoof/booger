c427be198673a26814b18a58051060d8f68f3ff8
Nathan Hwang <thenoviceoof@gmail.com>
2014-05-17 14:36:58 -0400

    Bump patch version (v0.3.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='booger',
-    version='0.3.0',
+    version='0.3.1',
     author='thenoviceoof',
     author_email='thenoviceoof@gmail.com',
     packages=['booger'],
